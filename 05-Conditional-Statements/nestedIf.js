a6206605c6788aaee44d183f9e9af5c253985d6e
Nupur-Mhatre3108 <nupurmhatre225@gmail.com>
2026-08-07 23:22:49 +0530

    Add JavaScript nested if statement examples

---
+++ 05-Conditional-Statements/nestedIf.js
@@ -0,0 +1,19 @@
+// Example 1:
+let age = 20;
+let hasLicense = true;
+
+if (age >= 18) {
+    if (hasLicense) {
+        console.log("You Can Drive");
+    }
+}
+//Output: You Can Drive
+
+//Example 2:
+let marks = 85;
+
+if (marks >= 40) {
+    if (marks >= 75) {
+        console.log("Distinction");
+    }
+}
