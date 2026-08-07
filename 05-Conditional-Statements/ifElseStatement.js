3c3ac8a3f1df5e3321d921a292d094090e76555e
Nupur-Mhatre3108 <nupurmhatre225@gmail.com>
2026-08-07 22:10:40 +0530

    Add JavaScript if-else statement examples

---
+++ 05-Conditional-Statements/ifElseStatement.js
@@ -0,0 +1,29 @@
+//Example 1:
+let age = 20;
+
+if (age >= 18) {
+    console.log("Eligible to Vote");
+} else {
+    console.log("Not Eligible to Vote");
+}
+//Output: Eligible to Vote
+
+//Example 2
+let age = 15;
+
+if (age >= 18) {
+    console.log("Eligible to Vote");
+} else {
+    console.log("Not Eligible to Vote");
+}
+//Output: Not Eligible to Vote
+
+//Example 3
+let number = 8;
+
+if (number % 2 === 0) {
+    console.log("Even Number");
+} else {
+    console.log("Odd Number");
+}
+//Output:Even Number
