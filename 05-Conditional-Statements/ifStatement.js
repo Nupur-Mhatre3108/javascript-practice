2c7baa8afe879f428257b6b9db8d1b4cd46667af
Nupur-Mhatre3108 <nupurmhatre225@gmail.com>
2026-08-07 22:02:35 +0530

    Add JavaScript if statement examples

---
+++ 05-Conditional-Statements/ifStatement.js
@@ -0,0 +1,17 @@
+let age = 20;
+
+if (age >= 18) {
+    console.log("Eligible to Vote");//Eligible to vote
+}
+
+let isLoggedIn = true;
+
+if (isLoggedIn) {
+    console.log("Welcome");//Welcome
+}
+
+let number = 10;
+
+if (number % 2 === 0) {
+    console.log("Even Number");//Even Number
+}
