226563f1f10ac15e8348f4f2b4ddbacd22a6a504
Nupur-Mhatre3108 <nupurmhatre225@gmail.com>
2026-08-07 23:04:22 +0530

    Add JavaScript else-if ladder examples

---
+++ 05-Conditional-Statements/elseIfLadder.js
@@ -0,0 +1,30 @@
+//Example 1:
+let marks = 55;
+
+if (marks >= 90) {
+    console.log("Grade A");
+}
+else if (marks >= 75) {
+    console.log("Grade B");
+}
+else if (marks >= 60) {
+    console.log("Grade C");
+}
+else {
+    console.log("Fail");
+}
+//Output:Fail
+
+//Example 2:
+let age = 65;
+
+if (age >= 60) {
+    console.log("Senior Citizen");
+}
+else if (age >= 18) {
+    console.log("Adult");
+}
+else {
+    console.log("Minor");
+}
+//Output: Senior citizen
