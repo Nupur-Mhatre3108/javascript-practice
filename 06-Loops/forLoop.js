3fca7e79ed0aab54254032d6f57b9520bbdebf51
Nupur-Mhatre3108 <nupurmhatre225@gmail.com>
2026-08-08 11:11:17 +0530

    Add JavaScript for loop examples

---
+++ 06-Loops/forLoop.js
@@ -0,0 +1,35 @@
+// Example 1 - Counting Forward
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+/*Output:
+1 
+2
+3
+4
+5
+*/
+
+// Example 2 - Counting Backward
+for (let i = 5; i >= 1; i--) {
+    console.log(i);
+}
+/* Output:
+5 
+4
+3
+2
+1
+*/
+
+// Example 3 - Even Numbers
+for (let i = 2; i <= 10; i += 2) {
+    console.log(i);
+}
+/* Output:
+2
+4 
+6
+8
+10
+*/
