72483a77ebc05ddb6b80a40ec9a7dd27889964c5
Nupur-Mhatre3108 <nupurmhatre225@gmail.com>
2026-08-08 11:29:15 +0530

    Add JavaScript while loop examples

---
+++ 06-Loops/whileLoop.js
@@ -0,0 +1,44 @@
+// Example 1 - Counting Forward
+let i = 1;
+
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
+/*Output:
+1 
+2
+3
+4
+5
+*/
+
+// Example 2 - Counting Backward
+let number = 5;
+
+while (number >= 1) {
+    console.log(number);
+    number--;
+}
+/* Output:
+5 
+4
+3
+2
+1
+*/
+
+// Example 3 - Even Numbers
+let num = 2;
+
+while (num <= 10) {
+    console.log(num);
+    num += 2;
+}
+/* Output:
+2
+4 
+6
+8
+10
+*/
